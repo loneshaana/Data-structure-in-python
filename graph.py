44ac53361b707a672e759448d14120bacf62acb8
Shaan <anwarulhaq794@gmail.com>
2017-12-12 14:12:00 +0530

    Graph
    Commit 1

---
+++ graph.py
@@ -0,0 +1,23 @@
+
+def find_path(graph,start,end,path=[]):
+	path = path + [start]
+	if start == end:
+		return path
+	if not graph.has_key(start):
+		return None
+	if end in graph[start]:
+		path = path + [end]
+		return path
+	else:
+		for node in graph[start]:
+			if node not in path:
+				newpath = find_path(graph,node,end,path)
+				if newpath:
+					return newpath
+		return None
+
+
+#----------Drive Code---------
+graph={'A':['B','C'],'B':['E','D'],'C':['D','F'],'E':['F']}
+
+print find_path(graph,'A','D')
